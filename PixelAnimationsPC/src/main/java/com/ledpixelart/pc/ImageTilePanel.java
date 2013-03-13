bb2bf1a42e2f7cc7fa4edda36ea2faa37fa69d0f
onebeartoe <onebeartoe@gmail.com>
2013-03-13 15:20:41 -0500

    The image path was corrected once more.

--- PixelAnimationsPC/src/main/java/com/ledpixelart/pc/ImageTilePanel.java
+++ PixelAnimationsPC/src/main/java/com/ledpixelart/pc/ImageTilePanel.java
@@ -6,7 +6,6 @@ import ioio.lib.api.exception.ConnectionLostException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -25,7 +24,7 @@ public class ImageTilePanel extends PixelTilePanel
     
     protected String imageListPath = "/images.text";
     
-    private String animation_name;
+    private String command;
     
     private static ActionListener animateTimer = null;
     
@@ -44,7 +43,6 @@ public class ImageTilePanel extends PixelTilePanel
 		    //only go here if PIXEL wa found, other leave the timer
 		    return;
 		}
-		// System.out.println("animate");
 
 		i++;
 
@@ -53,8 +51,7 @@ public class ImageTilePanel extends PixelTilePanel
 		    i = 0;
 		}
 
-		String animationName = evt.getActionCommand();
-		// framestring = "animations/decoded/boat/boat" + i + ".rgb565";
+		String animationName = command;		
 		String framestring = "/images/" + animationName + ".rgb565";
 		try 
 		{
@@ -66,76 +63,16 @@ public class ImageTilePanel extends PixelTilePanel
 		    // TODO Auto-generated catch block
 		    e1.printStackTrace();
 		}
-
-		// if (i == numFrames - 1) {
-		// Animate.restart();
-		//  }
-
 	    }
 	};
     }
     
     @Override
     public void actionPerformed(ActionEvent event) 
-    {
-	
-	String command = event.getActionCommand();
-	System.out.println("image comamand: " + command);
-	
-	String selectedFileName = event.getActionCommand();
-	String decodedDirPath = "animations/decoded";
-
-	//System.out.println("selected file name: " + selectedFileName);
-
-	InputStream decodedFile = PixelApp.class.getClassLoader().getResourceAsStream(decodedDirPath + "/" + selectedFileName + "/" + selectedFileName + ".txt"); //decoded/rain/rain.text
-	//note can't use file operator here as you can't reference files from a jar file
-
-	int selectedFileTotalFrames = 1;
+    {	
+	command = event.getActionCommand();
+	System.out.println("image comamand: " + command);	
 	int selectedFileDelay = 100;
-		
-	if (decodedFile != null) 
-	{
-	    
-	    // ok good, now let's read it, we need to get the total numbers of frames and the frame speed
-	    String line = null;
-	    try 
-	    {
-		//  BufferedReader br = new BufferedReader(new FileReader(decodedFile));
-		BufferedReader br = new BufferedReader(
-			new InputStreamReader(decodedFile));
-
-		line = br.readLine();
-
-		// while ((line = br.readLine()) != null) {
-		//     text.append(line);
-		//    text.append('\n');	   	             
-		// }
-	    } catch (IOException e) {
-		//You'll need to add proper error handling here
-	    }
-
-	    String fileAttribs = line.toString();  //now convert to a string	 
-	    //System.out.println(fileAttribs);
-	    String fdelim = "[,]"; //now parse this string considering the comma split  ie, 32,60
-	    String [] fileAttribs2 = fileAttribs.split(fdelim);
-	    selectedFileTotalFrames = Integer.parseInt(fileAttribs2[0].trim());
-
-	    //System.out.println("total frames: " + selectedFileTotalFrames);
-	    //System.out.println(fileAttribs2[0] + " " + fileAttribs2[1] + fileAttribs2[2]);
-
-	    selectedFileDelay = Integer.parseInt(fileAttribs2[1].trim());
-	    //selectedFileResolution = 32;
-	    //selectedFileResolution = Integer.parseInt(fileAttribs2[2].trim());
-	}
-
-	// System.out.println(fileAttribs);
-
-	//****** Now let's setup the animation ******
-	int i = 0;
-	animation_name = event.getActionCommand();
-	int numFrames = selectedFileTotalFrames;
-	// System.out.println("file delay: " + selectedFileDelay);
-
 	Timer timer = new Timer(selectedFileDelay, animateTimer);
 
 	if (timer.isRunning() == true) 
@@ -144,7 +81,6 @@ public class ImageTilePanel extends PixelTilePanel
 	}
 	timer.start();
     }    
-    
 	
     @Override
     protected ImageIcon getImageIcon(String pathName) 
