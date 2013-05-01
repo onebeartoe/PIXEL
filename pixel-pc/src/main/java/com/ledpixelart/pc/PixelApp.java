d7a229ab3d558ee525b93a4858f7e311c5232b64
alinke <alinke@yahoo.com>
2013-04-30 21:05:28 -0700

    uncommented the user defined tab

--- pixel-pc/src/main/java/com/ledpixelart/pc/PixelApp.java
+++ pixel-pc/src/main/java/com/ledpixelart/pc/PixelApp.java
@@ -7,6 +7,8 @@ import com.ledpixelart.pc.plugins.swing.ImageTilePanel;
 import com.ledpixelart.pc.plugins.swing.PixelPanel;
 import com.ledpixelart.pc.plugins.swing.PixelTilePanel;
 import com.ledpixelart.pc.plugins.swing.ScrollingTextPanel;
+import com.ledpixelart.pc.plugins.swing.UserProvidedPanel;
+
 import ioio.lib.api.AnalogInput;
 import ioio.lib.api.DigitalOutput;
 import ioio.lib.api.IOIO;
@@ -24,12 +26,12 @@ import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -57,8 +59,7 @@ public class PixelApp extends IOIOSwingApp
     
     private List<PixelPanel> imagePanels;
     
-    private static IOIO ioiO;
-    
+    private static IOIO ioiO; 
     private JFrame frame;
     
     private static RgbLedMatrix.Matrix KIND = ioio.lib.api.RgbLedMatrix.Matrix.SEEEDSTUDIO_32x32;
@@ -68,7 +69,7 @@ public class PixelApp extends IOIOSwingApp
     public PixelApp()
     {
 	logger = Logger.getLogger(PixelApp.class.getName());
-	
+
 	imagePanels = new ArrayList();	
     }
 
@@ -103,13 +104,14 @@ public class PixelApp extends IOIOSwingApp
 	//JFrame frame = new JFrame("PIXEL");
 	frame = new JFrame("PIXEL");
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
-	
+
 	JTabbedPane tabbedPane = new JTabbedPane();
 	String path = "/images/onion.png";
 	URL url = getClass().getResource(path);
-        ImageIcon icon = new ImageIcon(url);
+       ImageIcon icon = new ImageIcon(url);
 //        ImageIcon icon = createImageIcon("images/middle.png");
-	
+
+
 	PixelTilePanel imagesPanelReal = new ImageTilePanel(pixel.KIND);
 	imagesPanelReal.populate();
 	imagePanels.add(imagesPanelReal);
@@ -122,8 +124,8 @@ public class PixelApp extends IOIOSwingApp
         tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
     
     // temp commented out this tab until working    
-	String path = System.getProperty("user.home");
-    File homeDirectory = new File(path);
+	String path2 = System.getProperty("user.home");
+    File homeDirectory = new File(path2);
 	userTilePanel = new UserProvidedPanel(pixel.KIND, homeDirectory);
 	userTilePanel.populate();
 	imagePanels.add(userTilePanel);
@@ -144,7 +146,7 @@ public class PixelApp extends IOIOSwingApp
 		{
 		    panel.stopPixelActivity();
 		}
-		
+
 		// start the selected panel/tab's PIXEL activity
 		Object o = e.getSource();
 		JTabbedPane tabs = (JTabbedPane) o;
@@ -154,17 +156,17 @@ public class PixelApp extends IOIOSwingApp
             }
         });
 
-	JMenuBar menuBar = createMenuBar();
-	
+	//JMenuBar menuBar = createMenuBar();
+
 	frame.add(tabbedPane, BorderLayout.CENTER);	
 	frame.setSize(450, 600);		
-	frame.setJMenuBar(menuBar);
-	
+	//frame.setJMenuBar(menuBar);
+
 	// center it
 	frame.setLocationRelativeTo(null); 
-	
+
 	frame.setVisible(true);
-	
+
 	return frame;
     }
     
@@ -182,35 +184,28 @@ public class PixelApp extends IOIOSwingApp
 	// Build the first menu.
 	menu = new JMenu("Application");
 	menu.setMnemonic(KeyEvent.VK_A);
-	menu.getAccessibleContext().setAccessibleDescription("update with accessible description");
+	menu.getAccessibleContext().setAccessibleDescription(
+		"The only menu in this program that has menu items");
 	menuBar.add(menu);
-	
-	String path = "/images/apple.png";
-	URL url = getClass().getResource(path);
-	ImageIcon menuIcon = new ImageIcon(url);
 
 	// a group of JMenuItems
-	menuItem = new JMenuItem("About", menuIcon);
-	KeyStroke aboutKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK);
-	menuItem.setAccelerator(aboutKeyStroke);
-	menuItem.getAccessibleContext().setAccessibleDescription("update with accessible description");
-	menuItem.addActionListener( new AboutListener() );
+	menuItem = new JMenuItem("A text-only menu item",
+				 KeyEvent.VK_T);
+	menuItem.setAccelerator(KeyStroke.getKeyStroke(
+		KeyEvent.VK_1, ActionEvent.ALT_MASK));
+	menuItem.getAccessibleContext().setAccessibleDescription(
+		"This doesn't really do anything");
 	menu.add(menuItem);
 
-	
-	menuItem = new JMenuItem("Quit", menuIcon);
-	KeyStroke keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_2, ActionEvent.ALT_MASK);
-	menuItem.setAccelerator(keyStroke);
-	menuItem.addActionListener( new QuitListener() );
-//	menuItem.setMnemonic(KeyEvent.VK_B);
-	menuItem.getAccessibleContext().setAccessibleDescription("update with accessible description");
+	menuItem = new JMenuItem("Both text and icon",
+				 new ImageIcon("images/middle.gif"));
+	menuItem.setMnemonic(KeyEvent.VK_B);
 	menu.add(menuItem);
 
 	menuItem = new JMenuItem(new ImageIcon("images/middle.gif"));
 	menuItem.setMnemonic(KeyEvent.VK_D);
 	menu.add(menuItem);
 
-/*	
 	// a group of radio button menu items
 	menu.addSeparator();
 	ButtonGroup group = new ButtonGroup();
@@ -248,16 +243,13 @@ public class PixelApp extends IOIOSwingApp
 	menuItem = new JMenuItem("Another item");
 	submenu.add(menuItem);
 	menu.add(submenu);
-*/
-	
+
 	//Build second menu in the menu bar.
-/*	
 	menu = new JMenu("Plugins");
 	menu.setMnemonic(KeyEvent.VK_N);
 	menu.getAccessibleContext().setAccessibleDescription("This menu does nothing");
 	menuBar.add(menu);
-*/
-	
+
 	return menuBar;
     }
 
@@ -291,10 +283,9 @@ public class PixelApp extends IOIOSwingApp
 		setPixelFound();
 		System.out.println("Found PIXEL: " + pixel.matrix + "\n");
 		System.out.println("You may now interact with the PIXEL\n");
-		
+
 		//TODO: Load something on startup
 
-// Use PixelApp.this instead of frame.		
 		JOptionPane.showMessageDialog(frame, "Found PIXEL: Click an image or animation");
 	    }	    
 	};
@@ -348,27 +339,6 @@ public class PixelApp extends IOIOSwingApp
         }
         
         return pixel.analogInput1;
-    }
-
-    private class AboutListener implements ActionListener
-    {
-
-	public void actionPerformed(ActionEvent e) 
-	{
-	    String message = "This is about this application.";	    
-	    JOptionPane.showMessageDialog(frame, message);
-	}
-	
-    }
-    
-    private class QuitListener implements ActionListener
-    {
-
-	public void actionPerformed(ActionEvent e) 
-	{
-	    System.exit(1);
-	}
-	
     }
     
 }
