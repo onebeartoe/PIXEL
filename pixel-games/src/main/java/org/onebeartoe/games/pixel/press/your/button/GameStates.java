6afd76289cc14fbec5a07e611933f45e48148af2
onebeartoe <onebeartoe@gmail.com>
2013-06-11 15:29:30 -0500

    Game states were added.

---
+++ pixel-games/src/main/java/org/onebeartoe/games/pixel/press/your/button/GameStates.java
@@ -0,0 +1,13 @@
+
+package org.onebeartoe.games.pixel.press.your.button;
+
+/**
+ * @author rmarquez
+ */
+public enum GameStates 
+{
+    NEW_GAME_CONFIG,
+    NEXT_PLAYERS_TURN,
+    END_OF_TURN,
+    END_OF_GAME
+}
