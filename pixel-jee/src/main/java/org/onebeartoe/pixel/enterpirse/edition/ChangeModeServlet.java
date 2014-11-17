79685426533abc5ac2180080c0450d238f81585e
onebeartoe <onebeartoe@gmail.com>
2014-11-16 19:09:49 -0600

    The code was updated to use java.util.Timer instead of java.swing.Timer when drawing to the Pixel.

--- pixel-jee/src/main/java/org/onebeartoe/pixel/enterpirse/edition/ChangeModeServlet.java
+++ pixel-jee/src/main/java/org/onebeartoe/pixel/enterpirse/edition/ChangeModeServlet.java
@@ -1,7 +1,18 @@
 
 package org.onebeartoe.pixel.enterpirse.edition;
 
+import ioio.lib.api.exception.ConnectionLostException;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -11,6 +22,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import static org.onebeartoe.pixel.enterpirse.edition.InitializationServlet.MATRIX_TYPE;
+import static org.onebeartoe.pixel.enterpirse.edition.InitializationServlet.PIXEL_KEY;
+import static org.onebeartoe.pixel.enterpirse.edition.InitializationServlet.PIXEL_TIMER_KEY;
+import static org.onebeartoe.pixel.enterpirse.edition.InitializationServlet.fontNames;
+import org.onebeartoe.pixel.hardware.Pixel;
+
 /**
  * @author Roberto Marquez
  */
@@ -18,6 +35,44 @@ import javax.servlet.http.HttpServletResponse;
 public class ChangeModeServlet extends HttpServlet
 {
     private Logger logger;
+
+    volatile protected Timer timer;
+        
+    private HashMap<String, Font> fonts;
+
+    private int x;
+        
+        /**
+     * Override this to perform any additional background drawing on the image that get sent to the PIXEL
+     * @param g2d 
+     */
+    protected void additionalBackgroundDrawing(Graphics2D g2d) throws Exception
+    {
+        
+    }    
+    
+    /**
+     * Override this to perform any additional foreground drawing on the image that get sent to the PIXEL
+     * @param g2d 
+     */
+    protected void additionalForegroundDrawing(Graphics2D g2d) throws Exception
+    {
+        
+    }
+    
+    @Override
+    public void destroy()
+    {
+        if(timer == null)
+        {
+            logger.log(Level.INFO, "The init servlet stopped with not timer set.");
+        }
+        else
+        {
+            timer.cancel();
+            logger.log(Level.INFO, "The init servlet stopped the Pixel timer.");
+        }
+    }
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -34,7 +89,9 @@ public class ChangeModeServlet extends HttpServlet
         {
             mode = pi.substring(1);
         }
-
+        
+        TimerTask drawTask = null;
+        
         String forward;
         switch(mode)
         {
@@ -46,9 +103,34 @@ public class ChangeModeServlet extends HttpServlet
             }
             default:
             {
+                drawTask = new TextScroller();
+                
                 // scrolling text
                 forward = "scrolling-text";
             }
+        }
+        
+        ServletContext servletContext = getServletContext();     
+        Timer timer = (Timer) servletContext.getAttribute(PIXEL_TIMER_KEY);     
+        if(timer == null)
+        {
+            logger.log(Level.INFO, "The timer was null on mode change.");
+        }
+        else
+        {
+            timer.cancel();
+        }
+        
+        if(drawTask == null)
+        {
+            logger.log(Level.INFO, "The drawTask was null on mode change.");
+        }
+        else
+        {
+            Date now = new Date();
+            timer = new Timer();//now, startAction);        
+            long refreshDelay = 500;
+            timer.schedule(drawTask, now, refreshDelay);            
         }
         
         forward = "/mode/" + forward + "/index.jsp";
@@ -61,11 +143,109 @@ public class ChangeModeServlet extends HttpServlet
         rd.forward(request, response);
     }
     
+    public String getText()
+    {
+	return "some text";
+    }
+    
     @Override
     public void init() throws ServletException 
     {
         super.init();
         
         logger = Logger.getLogger(getClass().getName());
+        
+        fonts = new HashMap();
+
+        x = 0;
     }
+    
+    public class TextScroller extends TimerTask//implements ActionListener
+    {
+        @Override
+        public void run()
+        {
+	    int delay = 200;//scrollSpeedSlider.getValue();	
+	    delay = 710 - delay;                            // al linke: added this so the higher slider value means faster scrolling
+	    
+//	    ChangeModeServlet.this.timer.setDelay(delay);
+	    
+            int w = 64;
+            int h = 64;
+	    
+            BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+            
+	    Color textColor = Color.GREEN;//colorPanel.getBackground();
+	    
+            Graphics2D g2d = img.createGraphics();
+            g2d.setPaint(textColor);
+                      
+            String fontFamily = fontNames[0];
+//            String fontFamily = fontFamilyChooser.getSelectedItem().toString();
+            
+            Font font = fonts.get(fontFamily);
+            if(font == null)
+            {
+                font = new Font(fontFamily, Font.PLAIN, 32);
+                fonts.put(fontFamily, font);
+            }            
+            
+            g2d.setFont(font);
+            
+            String message = getText();
+            
+            FontMetrics fm = g2d.getFontMetrics();
+            
+            int y = fm.getHeight();            
+
+            try 
+            {
+                additionalBackgroundDrawing(g2d);
+            } 
+            catch (Exception ex) 
+            {
+                logger.log(Level.SEVERE, null, ex);
+            }
+            
+            g2d.drawString(message, x, y);
+            
+            try 
+            {
+                additionalForegroundDrawing(g2d);
+            } 
+            catch (Exception ex) 
+            {
+                logger.log(Level.SEVERE, null, ex);
+            }
+            
+            g2d.dispose();
+System.out.println(".");
+
+            ServletContext servletContext = getServletContext();     
+            Pixel pixel = (Pixel) servletContext.getAttribute(PIXEL_KEY);
+            if(pixel != null)
+            {
+                try 
+                {  
+                    pixel.writeImagetoMatrix(img, MATRIX_TYPE.width, MATRIX_TYPE.height);
+                } 
+                catch (ConnectionLostException ex) 
+                {
+                    logger.log(Level.SEVERE, null, ex);
+                }                
+            }
+                        
+            int messageWidth = fm.stringWidth(message);            
+            int resetX = 0 - messageWidth;
+            
+            if(x == resetX)
+            {
+                x = w;
+            }
+            else
+            {
+                x--;
+            }
+        }
+    }    
 }
--- pixel-jee/src/main/java/org/onebeartoe/pixel/enterpirse/edition/InitializationServlet.java
+++ pixel-jee/src/main/java/org/onebeartoe/pixel/enterpirse/edition/InitializationServlet.java
@@ -9,17 +9,11 @@ import ioio.lib.util.BaseIOIOLooper;
 import ioio.lib.util.IOIOLooper;
 import ioio.lib.util.IOIOLooperProvider;
 import ioio.lib.util.pc.IOIOPcApplicationHelper;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
@@ -42,34 +36,27 @@ public class InitializationServlet extends HttpServlet implements IOIOLooperProv
 // REMOVE THIS FROM CLASS SCOPE    
 //    private static IOIO ioiO;
     
-    private static RgbLedMatrix.Matrix MATRIX_TYPE = RgbLedMatrix.Matrix.SEEEDSTUDIO_32x32;
+    public static RgbLedMatrix.Matrix MATRIX_TYPE = RgbLedMatrix.Matrix.SEEEDSTUDIO_32x32;
     
 //    public static final Pixel pixel = new Pixel(MATRIX_TYPE);
     
     private Timer searchTimer;
     
-    volatile protected Timer timer;
-    
     private String statusLabel;
     
     private Logger logger;
     
-    private HashMap<String, Font> fonts;
-    
     public static final String [] fontNames = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-
-// MOVE THIS TO THE SCROLING TEXT CLASS    
-    private int x;
     
-    private final String PIXEL_KEY = "PIXEL_KEY";
+    public static final String PIXEL_KEY = "PIXEL_KEY";
+    
+    public static final String PIXEL_TIMER_KEY = "PIXEL_TIMER_KEY";
     
     @Override
     public void init()
     {
         String className = InitializationServlet.class.getName();
         logger = Logger.getLogger(className);
-     
-        fonts = new HashMap();
         
         Pixel pixel = new Pixel(MATRIX_TYPE);
                 
@@ -77,9 +64,11 @@ public class InitializationServlet extends HttpServlet implements IOIOLooperProv
         ServletContext servletContext = getServletContext();     
         servletContext.setAttribute(PIXEL_KEY, pixel);
         
+        // save teh timer to application scope
+        Timer timer = null;
+        servletContext.setAttribute(PIXEL_TIMER_KEY, timer);
+        
 	startSearchTimer();
-    
-        x = 0;
         
         String [] args = {};
         try 
@@ -98,16 +87,6 @@ public class InitializationServlet extends HttpServlet implements IOIOLooperProv
     public void destroy()
     {
         searchTimer.stop();
-                
-        if(timer == null)
-        {
-            logger.log(Level.INFO, "The init servlet stopped with not timer set.");
-        }
-        else
-        {
-            timer.stop();
-            logger.log(Level.INFO, "The init servlet stopped the REFRESH? timer.");
-        }
     }
     
     @Override
@@ -298,116 +277,5 @@ public class InitializationServlet extends HttpServlet implements IOIOLooperProv
 		}
 	    }
 	}
-    }
-
-    public String getText()
-    {
-	return "some text";
-    }
-    
-    /**
-     * Override this to perform any additional background drawing on the image that get sent to the PIXEL
-     * @param g2d 
-     */
-    protected void additionalBackgroundDrawing(Graphics2D g2d) throws Exception
-    {
-        
-    }    
-    
-    /**
-     * Override this to perform any additional foreground drawing on the image that get sent to the PIXEL
-     * @param g2d 
-     */
-    protected void additionalForegroundDrawing(Graphics2D g2d) throws Exception
-    {
-        
-    }
-    
-    private class TextScroller implements ActionListener
-    {
-        public void actionPerformed(ActionEvent e) 
-        {
-	    int delay = 200;//scrollSpeedSlider.getValue();	
-	    delay = 710 - delay;                            // al linke: added this so the higher slider value means faster scrolling
-	    
-	    InitializationServlet.this.timer.setDelay(delay);
-	    
-            int w = 64;
-            int h = 64;
-	    
-            BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-            
-	    Color textColor = Color.GREEN;//colorPanel.getBackground();
-	    
-            Graphics2D g2d = img.createGraphics();
-            g2d.setPaint(textColor);
-                      
-            String fontFamily = fontNames[0];
-//            String fontFamily = fontFamilyChooser.getSelectedItem().toString();
-            
-            Font font = fonts.get(fontFamily);
-            if(font == null)
-            {
-                font = new Font(fontFamily, Font.PLAIN, 32);
-                fonts.put(fontFamily, font);
-            }            
-            
-            g2d.setFont(font);
-            
-            String message = getText();
-            
-            FontMetrics fm = g2d.getFontMetrics();
-            
-            int y = fm.getHeight();            
-
-            try 
-            {
-                additionalBackgroundDrawing(g2d);
-            } 
-            catch (Exception ex) 
-            {
-                logger.log(Level.SEVERE, null, ex);
-            }
-            
-            g2d.drawString(message, x, y);
-            
-            try 
-            {
-                additionalForegroundDrawing(g2d);
-            } 
-            catch (Exception ex) 
-            {
-                logger.log(Level.SEVERE, null, ex);
-            }
-            
-            g2d.dispose();
-System.out.println(".");
-
-            ServletContext servletContext = getServletContext();     
-            Pixel pixel = (Pixel) servletContext.getAttribute(PIXEL_KEY);
-            if(pixel != null)
-            {
-                try 
-                {  
-                    pixel.writeImagetoMatrix(img, MATRIX_TYPE.width, MATRIX_TYPE.height);
-                } 
-                catch (ConnectionLostException ex) 
-                {
-                    logger.log(Level.SEVERE, null, ex);
-                }                
-            }
-                        
-            int messageWidth = fm.stringWidth(message);            
-            int resetX = 0 - messageWidth;
-            
-            if(x == resetX)
-            {
-                x = w;
-            }
-            else
-            {
-                x--;
-            }
-        }        
     }
 }
